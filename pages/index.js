2c457b612feecc1c3473de14e6749bb638b29751
odafeaggreh <aggrehodafe2020@outlook.com>
2021-12-05 07:02:42 -0800

    my first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>base</h1>
+    </div>
+  );t
+}
